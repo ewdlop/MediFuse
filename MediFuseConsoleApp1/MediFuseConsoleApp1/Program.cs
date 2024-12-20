5824ec42ba571cb3c9b8f3c2a636edaea3276cae
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-20 13:42:56 -0500

    DepthFirstSearch,BreathFirstSearch,DepthLimitedSearch, IterativeDeepeningDepthFirstSearch and use linq for filtering

--- MediFuseConsoleApp1/MediFuseConsoleApp1/Program.cs
+++ MediFuseConsoleApp1/MediFuseConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 using static MediFuseConsoleApp1.Tree;
 
 Console.WriteLine("Hello Ray!");
-//Console.WriteLine("Inorder: " + string.Join(", ", ino));
+//Console.WriteLine("In-order: " + string.Join(", ", ino));
 
 //Construct a simple tree:
 //        10
@@ -10,33 +10,63 @@ Console.WriteLine("Hello Ray!");
 //      5   15
 //     / \    \
 //    2   7    20
+Console.WriteLine("Constructing a simple tree:");
+Console.WriteLine("       10");
+Console.WriteLine("      /  \\");
+Console.WriteLine("     5   15");
+Console.WriteLine("    / \\    \\");
+Console.WriteLine("   2   7    20");
 
 INode<int> root = new Node<int>(10, [
     new Node<int>(5,[ new Node<int>(2, []),new Node<int>(7, [])]),
     new Node<int>(15,[new Node<int>(20, [])])]);
 
-// Preorder traversal
+// Pre-order traversal
 List<int> pre = root.Preorder().ToList();
-// Inorder traversal
-List<int> ino = root.Inorder().ToList();
-// Postorder traversal
-List<int> post = root.Postorder().ToList();
+Console.WriteLine($"Pre-order: {string.Join(", ", pre)}");
+// In-order traversal
+List<int> ino = root.InOrder().ToList();
+Console.WriteLine($"In-order: {string.Join(", ", ino)}");
+// Post-order traversal
+List<int> post = root.PostOrder().ToList();
+Console.WriteLine($"Post-order: {string.Join(", ", post)}");
+// Depth First traversal
+List<int> dfs = root.DepthFirstSearch().ToList();
+Console.WriteLine($"Depth First Search: {string.Join(", ", dfs)}");
+// Breadth First traversal
+List<int> bfs = root.BreadthFirstSearch().ToList();
+Console.WriteLine($"Breadth First Search: {string.Join(", ", bfs)}");
+// Depth Limited Search
+List<int> dls = root.DepthLimitedSearchTraverse().ToList();
+Console.WriteLine($"Depth Limited Search(No Depth Limit): {string.Join(", ", dls)}");
+// Iterative Deepening Depth First Search
+List<int> iddfs = root.IterativeDeepeningDepthFirstSearch().ToList();
+Console.WriteLine($"Iterative Deepening Depth First Search: {string.Join(", ", iddfs)}");
 
-Console.WriteLine($"Preorder: {string.Join(", ", pre)}");
-Console.WriteLine($"Inorder: {string.Join(", ", ino)}");
-Console.WriteLine($"Postorder: {string.Join(", ", post)}");
 
 // Using LINQ selector on a preorder traversal to select only even values
 List<int> evenValues = root.Traverse(Tree.Preorder)
                      .Where(x => x % 2 == 0)
                      .ToList();
-Console.WriteLine($"We are the Even values in Preorder: {string.Join(", ", evenValues)}");
 
+Console.WriteLine($"We are the even values in Preorder: {string.Join(", ", evenValues)}");
 
+// Using LINQ selector on a depth first search traversal to search a list of values
+List<int> searchValues = root.Traverse(Tree.DepthFirstSearch)
+                     .Where(x => x == 7 || x == 20)
+                     .ToList();
+Console.WriteLine($"We are the search values in Depth First Search: {string.Join(", ", searchValues)}");
+
+//Console output:
 /***
 Hello Ray!
-Preorder: 10, 5, 2, 7, 15, 20
-Inorder: 5, 2, 7, 10, 15, 20
-Postorder: 2, 7, 5, 20, 15, 10
-We are the Even values in in Preorder: 10, 2, 20
+Pre-order: 10, 5, 2, 7, 15, 20
+In-order: 5, 2, 7, 10, 15, 20
+Post-order: 2, 7, 5, 20, 15, 10
+Depth First Search: 10, 15, 20, 5, 7, 2
+Breadth First Search: 10, 5, 15, 2, 7, 20
+Depth Limited Search: 10, 5, 2, 7, 15, 20
+Iterative Deepening Depth First Search: 10, 5, 15, 2, 7, 20
+We are the Even values in Preorder: 10, 2, 20
+We are the search values in Depth First Search: 20, 7
 ***/
--- MediFuseConsoleApp1/MediFuseConsoleApp1/Tree.cs
+++ MediFuseConsoleApp1/MediFuseConsoleApp1/Tree.cs
@@ -35,7 +35,12 @@ public static partial class Tree
         public bool IsEndOfWord { get; set; } = IsEndOfWord;
     }
 
-    // Preorder Traversal: Root -> Left -> Right
+    /// <summary>
+    /// Pre-order Traversal: Root -> Left -> Right
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
     public static IEnumerable<T?> Preorder<T>(this INode<T>? node)
     {
         if (node is null) yield break;
@@ -46,8 +51,13 @@ public static partial class Tree
         }
     }
 
-    // Inorder Traversal: Left -> Root(mid) -> Right
-    public static IEnumerable<T?> Inorder<T>(this INode<T>? node)
+    /// <summary>
+    /// In-order Traversal: Left -> Root(mid) -> Right
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<T?> InOrder<T>(this INode<T>? node)
     {
         if (node is null) yield break;
         int mid = node.Nodes.Length / 2;
@@ -63,17 +73,141 @@ public static partial class Tree
         }
     }
 
-    // Postorder Traversal: Children -> Root
-    public static IEnumerable<T?> Postorder<T>(this INode<T>? node)
+    /// <summary>
+    /// Post-order Traversal: Children -> Root
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<T?> PostOrder<T>(this INode<T>? node)
     {
         if (node is null) yield break;
         for (int i = 0; i < node.Nodes.Length; i++)
         {
-            foreach (T? val in Postorder(node.Nodes[i])) yield return val;
+            foreach (T? val in PostOrder(node.Nodes[i])) yield return val;
         }
         yield return node.Value;
     }
     
+    /// <summary>
+    /// Depth First Search (DFS) Traversal
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<T?> DepthFirstSearch<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        Stack<INode<T>> stack = new();
+        stack.Push(node);
+        while (stack.Count > 0)
+        {
+            INode<T> current = stack.Pop();
+            yield return current.Value;
+            foreach (INode<T> child in current.Nodes)
+            {
+                stack.Push(child);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Breadth First Search (BFS) Traversal
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<T?> BreadthFirstSearch<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        Queue<INode<T>> queue = new();
+        queue.Enqueue(node);
+        while (queue.Count > 0)
+        {
+            INode<T> current = queue.Dequeue();
+            yield return current.Value;
+            foreach (INode<T> child in current.Nodes)
+            {
+                queue.Enqueue(child);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Depth Limited Search (DLS) Traversal
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <param name="depth"></param>
+    /// <returns></returns>
+    private static IEnumerable<T?> DepthLimitedSearch<T>(INode<T>? node, int? depth = null)
+    {
+        if (node is null) yield break;
+        if (depth == 0)
+        {
+            yield return node.Value;
+        }
+        else if (depth > 0)
+        {
+            foreach (INode<T> child in node?.Nodes?? Enumerable.Empty<INode<T>>())
+            {
+                foreach (T? val in DepthLimitedSearch(child, depth - 1))
+                {
+                    yield return val;
+                }
+            }
+        }
+        else if(depth is null)
+        {
+            yield return node.Value;
+            foreach (INode<T> child in node?.Nodes ?? Enumerable.Empty<INode<T>>())
+            {
+                foreach (T? val in DepthLimitedSearch(child, depth))
+                {
+                    yield return val;
+                }
+            }
+        }
+        else if (depth < 0)
+        {
+            yield break;
+        }
+    }
+
+    /// <summary>
+    /// Iterative Deepening Depth First Search (IDDFS) Traversal
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<T?> IterativeDeepeningDepthFirstSearch<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        int depth = 0;
+        while (true)
+        {
+            bool hasNeighbors = false; // Flag to check if there are neighbors at the current depth level or not
+            foreach (T? val in DepthLimitedSearch(node, depth))
+            {
+                hasNeighbors = true;
+                yield return val;
+            }
+            // If there are no neighbors at the current depth level, break the loop, else increment the depth.
+            // This is the exit condition for the loop
+            // Else, the loop will continue to run indefinitely
+            if (!hasNeighbors) break;
+            depth++;
+        }
+    }
+
+    /// <summary>
+    /// Generic traversal function
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <param name="traversalFunc"></param>
+    /// <returns></returns>
     public static IEnumerable<TResult?> Traverse<T, TResult>(
         this INode<T> node,
         Func<INode<T>, IEnumerable<TResult>> traversalFunc)
@@ -81,21 +215,91 @@ public static partial class Tree
         return traversalFunc(node);
     }
 
+    /// <summary>
+    /// Pre-order traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
     public static IEnumerable<TResult?> PreOrderTraverse<TResult>(
         this INode<TResult?> node)
     {
         return Preorder(node);
     }
 
+    /// <summary>
+    /// In-order traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
     public static IEnumerable<TResult?> InOrderTraverse<TResult>(
         this INode<TResult?> node)
     {
-        return Inorder(node);
+        return InOrder(node);
     }
 
+
+    /// <summary>
+    /// Post-order traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
     public static IEnumerable<TResult?> PostOrderTraverse<TResult>(
         this INode<TResult?> node)
     {
-        return Postorder(node);
+        return PostOrder(node);
+    }
+
+    /// <summary>
+    /// Depth First Search traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<TResult?> DepthFirstSearchTraverse<TResult>(
+        this INode<TResult?> node)
+    {
+        return DepthFirstSearch(node);
+    }
+
+
+    /// <summary>
+    /// Breadth First Search traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<TResult?> BreathFirstSearchTraverse<TResult>(
+        this INode<TResult?> node)
+    {
+        return BreadthFirstSearch(node);
+    }
+
+
+    /// <summary>
+    /// Depth Limited Search traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <param name="depth"></param>
+    /// <returns></returns>
+    public static IEnumerable<TResult?> DepthLimitedSearchTraverse<TResult>(
+        this INode<TResult?> node, int? depth = null)
+    {
+        return DepthLimitedSearch(node, depth);
+    }
+
+
+    /// <summary>
+    /// Iterative Deepening Depth First Search traversal
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public static IEnumerable<TResult?> IterativeDeepeningDepthFirstSearchTraverse<TResult>(this INode<TResult?> node)
+    {
+        return IterativeDeepeningDepthFirstSearch(node);
     }
 }
