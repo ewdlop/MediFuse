80c62691a0f6d04c5b9a30108cbe81d1102a7b91
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-19 21:02:38 -0500

    TreeNode, TrieNode, and TreeTraversals

---
+++ MediFuseConsoleApp1/MediFuseConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using MediFuseConsoleApp1;
+using static MediFuseConsoleApp1.Tree;
+
+Console.WriteLine("Hello Ray!");
+//Console.WriteLine("Inorder: " + string.Join(", ", ino));
+
+//Construct a simple tree:
+//        10
+//       /  \
+//      5   15
+//     / \    \
+//    2   7    20
+
+INode<int> root = new Node<int>(10, [
+    new Node<int>(5,[ new Node<int>(2, []),new Node<int>(7, [])]),
+    new Node<int>(15,[new Node<int>(20, [])])]);
+
+// Preorder traversal
+List<int> pre = root.Preorder().ToList();
+// Inorder traversal
+List<int> ino = root.Inorder().ToList();
+// Postorder traversal
+List<int> post = root.Postorder().ToList();
+
+Console.WriteLine("Preorder: " + string.Join(", ", pre));
+Console.WriteLine("Inorder: " + string.Join(", ", ino));
+Console.WriteLine("Postorder: " + string.Join(", ", post));
+
+// Using LINQ selector on a preorder traversal to select only even values
+List<int> evenValues = root.Traverse(Tree.Preorder)
+                     .Where(x => x % 2 == 0)
+                     .ToList();
+Console.WriteLine("We are the Even values in Preorder: " + string.Join(", ", evenValues));
+
+
+/***
+Hello Ray!
+Preorder: 10, 5, 2, 7, 15, 20
+Inorder: 5, 2, 7, 10, 15, 20
+Postorder: 2, 7, 5, 20, 15, 10
+We are the Even values in in Preorder: 10, 2, 20
+***/
---
+++ MediFuseConsoleApp1/MediFuseConsoleApp1/Tree.cs
@@ -0,0 +1,101 @@
+﻿namespace MediFuseConsoleApp1;
+
+public static partial class Tree
+{
+    public interface IReadOnlyNode<T>
+    {
+        T? Value { get; }
+        IReadOnlyList<IReadOnlyNode<T>> AsReadOnly { get; }
+    }
+
+    public interface INode<T> : IReadOnlyNode<T>
+    {
+        INode<T>[] Nodes { get; }
+    }
+
+    public interface ITrieNode<T> : INode<T>
+    {
+        bool IsEndOfWord { get; }
+    }
+
+    public class Node<T>(T? Value, Node<T>[] Nodes) : INode<T>
+    {
+        public T? Value { get; set; } = Value;
+        public INode<T>[] Nodes { get; set; } = Nodes;
+
+        INode<T>[] INode<T>.Nodes => Nodes;
+
+        T? IReadOnlyNode<T>.Value => Value;
+
+        IReadOnlyList<IReadOnlyNode<T>> IReadOnlyNode<T>.AsReadOnly => Nodes.AsReadOnly();
+    }
+
+    public class TrieNode<T>(T? Value, Node<T>[] Nodes, bool IsEndOfWord) : Node<T>(Value, Nodes)
+    {
+        public bool IsEndOfWord { get; set; } = IsEndOfWord;
+    }
+
+    // Preorder Traversal: Root -> Left -> Right
+    public static IEnumerable<T?> Preorder<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        yield return node.Value;
+        for(int i = 0; i < node.Nodes.Length; i++)
+        {
+            foreach (T? val in Preorder<T>(node.Nodes[i])) yield return val;
+        }
+    }
+
+    // Inorder Traversal: Left -> Root(mid) -> Right
+    public static IEnumerable<T?> Inorder<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        int mid = node.Nodes.Length / 2;
+        for (int i = 0; i < mid; i++)
+        {
+            foreach (T? val in Preorder(node.Nodes[i])) yield return val;
+
+        }
+        yield return node.Value;
+        for (int i = mid; i < node.Nodes.Length; i++)
+        {
+            foreach (T? val in Preorder(node.Nodes[i])) yield return val;
+        }
+    }
+
+    // Postorder Traversal: Children -> Root
+    public static IEnumerable<T?> Postorder<T>(this INode<T>? node)
+    {
+        if (node is null) yield break;
+        for (int i = 0; i < node.Nodes.Length; i++)
+        {
+            foreach (T? val in Postorder(node.Nodes[i])) yield return val;
+        }
+        yield return node.Value;
+    }
+    
+    public static IEnumerable<TResult?> Traverse<T, TResult>(
+        this INode<T> node,
+        Func<INode<T>, IEnumerable<TResult>> traversalFunc)
+    {
+        return traversalFunc(node);
+    }
+
+    public static IEnumerable<TResult?> PreOrderTraverse<TResult>(
+        this INode<TResult?> node)
+    {
+        return Preorder(node);
+    }
+
+    public static IEnumerable<TResult?> InOrderTraverse<TResult>(
+        this INode<TResult?> node)
+    {
+        return Inorder(node);
+    }
+
+    public static IEnumerable<TResult?> PostOrderTraverse<TResult>(
+        this INode<TResult?> node)
+    {
+        return Postorder(node);
+    }
+}
